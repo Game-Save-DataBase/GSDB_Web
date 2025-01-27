1eea6307b9a1aa5515d9ce5e28af5e2803040bfc
jorgbell <jorgbell@ucm.es>
2025-01-27 13:04:15 +0100

    Lectura de screenshots a medias (los lee de la web, no de bbdd). finalizado por ahora showsavedetails

--- src/components/ShowSaveDetails.jsx
+++ src/components/ShowSaveDetails.jsx
@@ -7,9 +7,22 @@ import '../styles/ShowSaveDetails.scss';
 function ShowSaveDetails(props) {
   const [saveData, setSaveData] = useState({});
   const [relatedGame, setRelatedGame] = useState(null);
+  const [relatedUser, setRelatedUser] = useState(null);
   const [comments, setComments] = useState([]);
+  const [imagePaths, setImagePaths] = useState([]);
   const { id } = useParams();
   const navigate = useNavigate();
+
+  // Función para obtener las imágenes desde el servidor
+  const fetchImages = async (saveId) => {
+    try {
+      const response = await axios.get(`http://localhost:8082/api/savedatas/${id}/images`); setImagePaths(response.data);  // Aquí se actualizan las rutas de las imágenes
+      setImagePaths(response.data.images);
+      // console.log(response.data.images)
+    } catch (err) {
+      console.log('Error fetching images:', err);
+    }
+  };
 
   useEffect(() => {
     // Función para obtener los detalles del archivo de guardado
@@ -17,6 +30,7 @@ function ShowSaveDetails(props) {
       try {
         const saveResponse = await axios.get(`http://localhost:8082/api/savedatas/${id}`);
         setSaveData(saveResponse.data);
+        fetchImages(id)
       } catch (err) {
         console.log('Error fetching save data:', err);
       }
@@ -25,10 +39,9 @@ function ShowSaveDetails(props) {
     fetchSaveData();
   }, [id]);
 
-
   useEffect(() => {
     // Obtener comentarios y añadir usernames
-    const fetchCommentsWithUsernames = async () => {
+    const fetchComments = async () => {
       try {
         const commentsResponse = await axios.get(`http://localhost:8082/api/comments/entry/${id}`);
         const commentsData = commentsResponse.data;
@@ -52,7 +65,7 @@ function ShowSaveDetails(props) {
       }
     };
 
-    fetchCommentsWithUsernames();
+    fetchComments();
   }, [id]);
 
   useEffect(() => {
@@ -70,6 +83,23 @@ function ShowSaveDetails(props) {
       fetchGameData();
     }
   }, [saveData.gameID]); // Este useEffect se activa cuando saveData.gameID cambia
+
+  useEffect(() => {
+    // Solo ejecutar si userID está disponible
+    if (saveData.userID) {
+      const fetchUserData = async () => {
+        try {
+          const userResponse = await axios.get(`http://localhost:8082/api/users/${saveData.userID}`);
+          setRelatedUser(userResponse.data);
+        } catch (err) {
+          console.log('Error fetching user:', err);
+        }
+      };
+
+      fetchUserData();
+    }
+  }, [saveData.userID]); // Este useEffect se activa cuando saveData.gameID cambia
+
 
 
   return (
@@ -116,19 +146,25 @@ function ShowSaveDetails(props) {
               {/* Columna derecha: información del archivo */}
               <div className='row-element text-muted'>
                 <p>
-                  <strong>Platform:</strong> {saveData.platform || 'Plataforma desconocida'}
+                  <strong>Platform:</strong>{' '}
+                  {relatedGame && relatedGame.platformsID && saveData.platformID !== undefined
+                    ? relatedGame.platformsID[saveData.platformID] || 'Plataforma desconocida'
+                    : 'Plataforma desconocida'}
                 </p>
                 <p>
                   <strong>File size:</strong> {saveData.filseSize || 'Tamaño sin determinar'}
                 </p>
                 <p>
-                  <strong>Submitted By:</strong> {saveData.submittedBy || 'Desconocido'}
+                  <strong>Submitted By:</strong> {' '}
+                  {relatedUser
+                    ? relatedUser.handleName || relatedUser.name || 'Desconocido'
+                    : 'Desconocido'}
                 </p>
                 <p>
-                  <strong>Date added:</strong> {new Date(saveData.postedDate).toLocaleDateString() || 'N/A'}
+                  <strong>Date added:</strong> {saveData.postedDate || 'N/A'}
                 </p>
                 <p>
-                  <strong>Downloads:</strong> {saveData.downloads || 'Numero de descargas desconocido'}
+                  <strong>Downloads:</strong> {saveData.nDownloads || 0}
                 </p>
                 {/* <br></br> */}
                 <p>
@@ -139,7 +175,7 @@ function ShowSaveDetails(props) {
 
             <div className='row'>
               <div className='row-element text-center'>
-                <button type="button" class="gsdb-btn-default">Download</button>
+                <button type="button" className="gsdb-btn-default">Download</button>
               </div>
             </div>
           </div>
@@ -155,7 +191,7 @@ function ShowSaveDetails(props) {
                 <button className="active" data-bs-toggle="tab" data-bs-target="#comments">Comments</button>
               </li>
               <li role="presentation">
-                <button data-bs-toggle="tab" data-bs-target="#screenshots">Screenshots</button>
+                <button className={`${imagePaths.length <= 0 ? 'disabled' : ''}`} data-bs-toggle="tab" data-bs-target="#screenshots">Screenshots</button>
               </li>
             </ul>
           </div>
@@ -168,6 +204,9 @@ function ShowSaveDetails(props) {
               {comments && comments.length > 0 ? (
                 comments.map((comment, index) => (
                   <div key={index} className="comment">
+                    <p>
+                      <small>{comment.postedDate}</small>
+                    </p>
                     <img
                       src={comment.userAvatar}
                       alt={comment.handleName || comment.userName}
@@ -178,18 +217,32 @@ function ShowSaveDetails(props) {
                   </div>
                 ))
               ) : (
-                <p className="text-muted">No hay comentarios disponibles para este archivo.</p>
+                <p className="text-muted">No hay comentarios para este archivo.</p>
               )}
             </div>
 
             {/* SCREENSHOTS */}
             <div className="gsdbtab" id="screenshots">
-              aqui los screenshots pls</div>
+              {imagePaths.length > 0 ? (
+                imagePaths.map((imagePath, index) => (
+                  <img key={index}
+                    // src={`http://localhost:8082${imagePath}`} 
+                    src={`/src/${imagePath}`} //DEBERIA COGER ESTO DE BASE DE DATOS, PERO AUN NO SE HACERLO BIEN. HASTA QUE SE ARREGLE LO COGEMOS DE LA WEB PARA EVITAR ERRORES EN CONSOLA
+                    alt={`Screenshot ${index + 1}`} className="screenshot" />
+
+                ))
+              ) : (
+                <p>No hay imágenes disponibles para este guardado.</p>
+              )}
+
+            </div>
+
+
           </div>
         </section>
 
       </div>
-
+      {/* TO DO: AÑADIR MODAL PARA LAS SCREENSHOTS */}
     </div>
   );
 }
